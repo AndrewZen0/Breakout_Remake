6e544906f9a22f52404bc6005317caf1797ee238
Andrea Gammella <andreapsn98@gmail.com>
2021-11-28 15:36:33 +0100

    aggiunto muri ed aggiornato lo script del paddle per permetter alla palla di rimbalzare in diagonale

--- Breakout_Remake/Assets/Scipts/PaddleMovement.cs
+++ Breakout_Remake/Assets/Scipts/PaddleMovement.cs
@@ -36,4 +36,27 @@ public class PaddleMovement : MonoBehaviour
 
         transform.position = new Vector2(Mathf.Clamp(transform.position.x, -7.5f, 7.7f), -4);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            Rigidbody2D ballRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            Vector3 hitPoint = collision.contacts[0].point;
+            Vector3 paddleCenter = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y);
+
+            ballRb.velocity = Vector2.zero;
+
+            float difference = paddleCenter.x - hitPoint.x;
+
+            if (hitPoint.x < paddleCenter.x)
+            {
+                ballRb.AddForce(new Vector2(-(Mathf.Abs(difference * 200)), BallsManager.Instance.initialBallSpeed));
+            }
+            else
+            {
+                ballRb.AddForce(new Vector2((Mathf.Abs(difference * 200)), BallsManager.Instance.initialBallSpeed));
+            }
+        }
+    }
 }
