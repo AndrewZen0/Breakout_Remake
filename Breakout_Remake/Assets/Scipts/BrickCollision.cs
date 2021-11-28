8e5724c78cf796d5a3d62343c31569a959f9d354
Andrea Gammella <andreapsn98@gmail.com>
2021-11-28 16:24:46 +0100

    creato bricks

---
+++ Breakout_Remake/Assets/Scipts/BrickCollision.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickCollision : MonoBehaviour
+{
+    public int hitPoint = 1;
+
+    public static event Action<BrickCollision> OnBrickDestruction;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BallMovement ball = collision.gameObject.GetComponent<BallMovement>();
+        ApplyCollisionMechanic(ball);
+    }
+
+    private void ApplyCollisionMechanic(BallMovement ball)
+    {
+        this.hitPoint--;
+
+        if(this.hitPoint <= 0)
+        {
+            OnBrickDestruction?.Invoke(this);
+            Destroy(this.gameObject);
+        }
+    }
+}
